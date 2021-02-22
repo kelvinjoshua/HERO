1e0fa18ff93924c3bf1ced14bec6579a59128820
kelvinjoshua <kelvinbale78@gmail.com>
2021-02-23 00:25:59 +0300

    New template new-squad,to get user content

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,12 @@ public class App {
             return new ModelAndView(model,"first-view.hbs");
         }, new HandlebarsTemplateEngine());
 
+        /*Route to retrieve content inputted by the user*/
+        get("/squads/new",(request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "new-squad.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
 
 
