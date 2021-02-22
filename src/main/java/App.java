3186fc34c471093429b16ac6fddf8b737671da0b
kelvinjoshua <kelvinbale78@gmail.com>
2021-02-22 22:52:52 +0300

    Added root Route in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,4 +9,12 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    public  static void main(String[] args){
+        staticFileLocation("/public");
+        /*Route route for our app*/
+        get("/", (request, response) -> {
+            Map<String,Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    };
 }
